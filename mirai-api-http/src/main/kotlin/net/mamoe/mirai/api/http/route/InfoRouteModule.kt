0c0536fc634c71a93ecaccf1b08fab0dc01cc818
copydog <775150@qq.com>
2020-04-08 16:37:25 +0800

    添加文档

--- mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/route/InfoRouteModule.kt
+++ mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/route/InfoRouteModule.kt
@@ -17,23 +17,66 @@ import net.mamoe.mirai.api.http.data.common.MemberDTO
 import net.mamoe.mirai.api.http.data.common.QQDTO
 import net.mamoe.mirai.api.http.util.toJson
 import net.mamoe.mirai.contact.toMutableList
+import net.mamoe.mirai.utils.MiraiExperimentalAPI
 
+/**
+ * 基本信息路由
+ */
 fun Application.infoModule() {
     routing {
 
+        /**
+         * 查询好友列表
+         */
         miraiGet("/friendList") {
             val ls = it.bot.friends.toMutableList().map { qq -> QQDTO(qq) }
             call.respondJson(ls.toJson())
         }
 
+        /**
+         * 查询QQ群列表
+         */
         miraiGet("/groupList") {
             val ls = it.bot.groups.toMutableList().map { group -> GroupDTO(group) }
             call.respondJson(ls.toJson())
         }
 
+        /**
+         * 查询QQ群成员列表
+         */
         miraiGet("/memberList") {
             val ls = it.bot.getGroup(paramOrNull("target")).members.toMutableList().map { member -> MemberDTO(member) }
             call.respondJson(ls.toJson())
+        }
+
+        /**
+         * 查询机器人个人信息
+         */
+        miraiGet("/botProfile") {
+            // TODO: 等待queryProfile()支持
+            val profile = it.bot.selfQQ.queryProfile();
+            call.respondJson(profile.toJson())
+        }
+
+        /**
+         * 查询好友个人信息
+         */
+        miraiGet("/friendProfile") {
+            // TODO: 等待queryProfile()支持
+            val profile = it.bot.getFriend(paramOrNull("friendId")).queryProfile()
+            call.respondJson(profile.toJson())
+        }
+
+        /**
+         * 查询QQ群成员个人信息
+         */
+        miraiGet("/memberProfile") {
+            // TODO: 等待queryProfile()支持
+            val profile = it.bot
+                .getGroup(paramOrNull("groupId"))
+                .get(paramOrNull("memberId"))
+                .queryProfile()
+            call.respondJson(profile.toJson())
         }
     }
 }
