2e922499bf265a4a27eb63625b86b56b55896946
Him188 <Him188@mamoe.net>
2020-05-07 10:23:46 +0800

    Update copyright

--- mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/service/report/ReportService.kt
+++ mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/service/report/ReportService.kt
@@ -1,23 +1,28 @@
+/*
+ * Copyright 2020 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/master/LICENSE
+ */
+
 package net.mamoe.mirai.api.http.service.report
 
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import net.mamoe.mirai.api.http.data.common.IgnoreEventDTO
 import net.mamoe.mirai.api.http.data.common.toDTO
 import net.mamoe.mirai.api.http.service.MiraiApiHttpService
-import net.mamoe.mirai.api.http.service.MiraiApiHttpServices
 import net.mamoe.mirai.api.http.util.HttpClient
 import net.mamoe.mirai.api.http.util.toJson
-import net.mamoe.mirai.console.plugins.Config
 import net.mamoe.mirai.console.plugins.PluginBase
 import net.mamoe.mirai.event.Listener
 import net.mamoe.mirai.event.events.BotEvent
 import net.mamoe.mirai.event.subscribeAlways
-import net.mamoe.mirai.event.subscribeGroupMessages
-import net.mamoe.mirai.event.subscribeMessages
 import net.mamoe.mirai.message.ContactMessage
 import net.mamoe.mirai.message.FriendMessage
 import net.mamoe.mirai.message.GroupMessage
+import net.mamoe.mirai.utils.error
 
 /**
  * 上报服务
@@ -43,7 +48,7 @@ class ReportService(console: PluginBase) : MiraiApiHttpService {
     }
 
     override fun onEnable() {
-        subscription = console.subscribeAlways<BotEvent> {
+        subscription = console.subscribeAlways {
             this.takeIf { reportConfig.enable }
                 ?.apply {
                     this.takeIf { reportConfig.eventMessage.report }
@@ -95,7 +100,7 @@ class ReportService(console: PluginBase) : MiraiApiHttpService {
         try {
             HttpClient.post(destination, json, reportConfig.extraHeaders)
         } catch (e: Exception) {
-            console.logger.error("上报${destination}失败: ${e.message}")
+            console.logger.error { "上报${destination}失败: ${e.message}" }
         }
     }
 }
--- mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/util/HttpClient.kt
+++ mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/util/HttpClient.kt
@@ -1,10 +1,21 @@
+/*
+ * Copyright 2020 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/master/LICENSE
+ */
+
 package net.mamoe.mirai.api.http.util
 
-import io.ktor.http.HttpMethod
-import io.ktor.client.request.*
 import io.ktor.client.request.forms.MultiPartFormDataContent
 import io.ktor.client.request.forms.formData
+import io.ktor.client.request.headers
+import io.ktor.client.request.request
+import io.ktor.client.request.url
 import io.ktor.http.ContentType
+import io.ktor.http.HttpMethod
 import io.ktor.http.content.PartData
 import io.ktor.http.content.TextContent
 import io.ktor.util.InternalAPI
@@ -15,7 +26,7 @@ import io.ktor.util.InternalAPI
 class HttpClient {
     companion object {
         /**
-         * 使用Ktor的HttpClient
+         * 使用 Ktor 的 [HttpClient]
          */
         private val client = io.ktor.client.HttpClient()
 
