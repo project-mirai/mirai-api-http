3235bfc568ef623cb17145e48faa0bce5807853a
ryoii <ryoii@foxmail.com>
2020-10-26 18:14:03 +0800

    Turn Report and HeartBeat Service off by default, close #196

--- mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/config/Setting.kt
+++ mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/config/Setting.kt
@@ -36,7 +36,7 @@ object Setting : AbstractPluginData(), PluginConfig {
      */
     @Serializable
     data class Report(
-        val enable: Boolean = true,
+        val enable: Boolean = false,
         val groupMessage: Reportable = Reportable(true),
         val friendMessage: Reportable = Reportable(true),
         val tempMessage: Reportable = Reportable(true),
@@ -57,7 +57,7 @@ object Setting : AbstractPluginData(), PluginConfig {
      */
     @Serializable
     data class HeartBeat(
-        val enable: Boolean = true,
+        val enable: Boolean = false,
         val delay: Long = 1000,
         val period: Long = 15000,
         val destinations: Destinations = emptyList(),
