1c479073570216be737d8641d31179b27ac7df1b
dousha <lijiahao34@gmail.com>
2020-10-29 12:23:42 +0800

    make artifact target on JRE 8 (while building with JDK11)

--- mirai-api-http/build.gradle.kts
+++ mirai-api-http/build.gradle.kts
@@ -110,11 +110,11 @@ kotlin {
 
 val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "1.8"
 }
 val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "1.8"
 }
 
 project.version = httpVersion
