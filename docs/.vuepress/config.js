300b8c80ad0534be18c7f3b687362170a480ef5c
Mix <admin@yami.im>
2021-01-30 11:33:04 +0800

    add navigation options and README.md in docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,34 @@
 module.exports = {
-  title: 'mirai-api-http',
-  description: 'Mirai HTTP API (console) plugin',
+  title: "mirai-api-http",
+  description: "Mirai HTTP API (console) plugin",
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   theme: "antdocs",
   themeConfig: {
-    sidebar: 'auto',
+    sidebar: "auto",
     sidebarDepth: 4,
-    displayAllHeaders: true
+    displayAllHeaders: true,
+    repo: "project-mirai/mirai-api-http",
+    logo: "https://raw.githubusercontent.com/mamoe/mirai/dev/docs/mirai.png",
+    docsDir: "docs",
+    editLinks: true,
+    smoothScroll: true,
+    editLinkText: "在 GitHub 上编辑此页",
+    lastUpdated: "上次更新",
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "API", link: "/API/" },
+      { text: "事件类型", link: "/EventType/" },
+      { text: "消息类型", link: "/MessageType/" },
+      {
+        text: "其他",
+        items: [
+          { text: "心跳服务", link: "/Heartbeat/" },
+          { text: "事件上报", link: "/Report/" },
+        ],
+      },
+    ],
   },
   plugins: [
     "@vuepress/plugin-medium-zoom",
@@ -18,9 +38,9 @@ module.exports = {
         serviceWorker: true,
         updatePopup: {
           message: "发现新内容",
-          buttonText: "刷新"
-        }
-      }
+          buttonText: "刷新",
+        },
+      },
     ],
-  ]
-}
+  ],
+};
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+---
+home: true
+heroImage: https://raw.githubusercontent.com/mamoe/mirai/dev/docs/mirai.png
+tagline: Mirai HTTP API (console) plugin
+actionText: API文档
+actionLink: API/
+features:
+  - title: 通用
+    details: Mirai-API-http插件 提供HTTP API供所有语言使用mirai
+footer: AGPL-3.0 License | Project Mirai
+---
