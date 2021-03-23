46814ecaa6b5f26b40a09a95695b606d30b1e06e
Doctor_Yin <1921841421@qq.com>
2021-03-23 19:03:07 +0800

    清理

--- mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/route/NudgeModule.kt
+++ mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/route/NudgeModule.kt
@@ -12,13 +12,12 @@ package net.mamoe.mirai.api.http.route
 import io.ktor.application.*
 import io.ktor.routing.*
 import kotlinx.serialization.Serializable
-import net.mamoe.mirai.api.http.data.IllegalAccessException
 import net.mamoe.mirai.api.http.data.StateCode
 import net.mamoe.mirai.api.http.data.common.VerifyDTO
 import net.mamoe.mirai.contact.getMember
 
 /**
- * 戳一戳发送（不知道放哪好...）
+ * 戳一戳发送
  */
 fun Application.nudgeModule() {
     routing {
@@ -29,25 +28,25 @@ fun Application.nudgeModule() {
         miraiVerify<NudgeDTO>("/sendNudge") { dto ->
             when (dto.environment) {
                 "Friend" -> {
-                    val friend = dto.session.bot.getFriend(dto.target) ?: throw IllegalAccessException("好友不存在")
+                    val friend = dto.session.bot.getFriend(dto.target) ?: throw IllegalArgumentException("好友不存在")
                     friend.nudge().sendTo(friend)
 
                 }
                 "Group" -> {
-                    val group = dto.session.bot.getGroup(dto.subject) ?: throw IllegalAccessException("群号不存在")
-                    val member = group.getMember(dto.target) ?: throw IllegalAccessException("群员不存在")
+                    val group = dto.session.bot.getGroup(dto.subject) ?: throw IllegalArgumentException("群号不存在")
+                    val member = group.getMember(dto.target) ?: throw IllegalArgumentException("群员不存在")
                     member.nudge().sendTo(member)
                 }
                 "Stranger" -> {
-                    val stranger = dto.session.bot.getStranger(dto.target) ?: throw IllegalAccessException("没这个陌生人")
+                    val stranger = dto.session.bot.getStranger(dto.target) ?: throw IllegalArgumentException("没这个陌生人")
                     stranger.nudge().sendTo(stranger)
                 }
 
                 "Bot" -> {
-                    val friend = dto.session.bot.getFriend(dto.target) ?: throw IllegalAccessException("好友不存在")
+                    val friend = dto.session.bot.getFriend(dto.target) ?: throw IllegalArgumentException("好友不存在")
                     dto.session.bot.nudge().sendTo(friend)
                 }
-                else -> throw IllegalAccessException("戳一戳类型不存在")
+                else -> throw IllegalArgumentException("戳一戳类型不存在")
             }
             call.respondStateCode(StateCode.Success)
         }
