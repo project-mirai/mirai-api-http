b0307363a58513b0ac3b6327cb132663679c32b8
mamoebot <mamoebot@users.noreply.github.com>
2021-04-03 12:12:58 +0000

    Update vuepress config (b8381dcc5d423b4e8aa4c0a3cbae592c15a37ca4)

--- service-worker.js
+++ service-worker.js
@@ -27,15 +27,15 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "d2e1fc80d440ee0ffda12f1a38a6516f"
+    "revision": "50f7cb6df4c219ceec08fa471513b077"
   },
   {
     "url": "API.html",
-    "revision": "659d759c924c98d631c33cb63730a61e"
+    "revision": "f227ad51817d099cb40415bf72ea156f"
   },
   {
-    "url": "assets/css/0.styles.f46ac581.css",
-    "revision": "3d76e72705323794d7506bfe2ab22931"
+    "url": "assets/css/0.styles.0ec3460c.css",
+    "revision": "319874252161608990e7c1f12e0f1e5f"
   },
   {
     "url": "assets/img/search.83621669.svg",
@@ -46,12 +46,12 @@ self.__precacheManifest = [
     "revision": "867d45d8f9c0da0e3e733dd5e7a8d263"
   },
   {
-    "url": "assets/js/10.9d2758e9.js",
-    "revision": "75eff01a47196549f3906c3809a1cc54"
+    "url": "assets/js/10.36ea21fb.js",
+    "revision": "bdc8a3d476a7864cc06a60f6ac0d3e40"
   },
   {
-    "url": "assets/js/11.36d9e40a.js",
-    "revision": "b1246b982625f6cb4bedbed2d568c8a4"
+    "url": "assets/js/11.2903950d.js",
+    "revision": "8c3e36601975ebcad9cf6c23dbd346bc"
   },
   {
     "url": "assets/js/12.d2794380.js",
@@ -86,32 +86,32 @@ self.__precacheManifest = [
     "revision": "810d737eb5b49e55ef8c20117c81965e"
   },
   {
-    "url": "assets/js/9.60a9ff71.js",
-    "revision": "f67ccd542cde791be68449e5343a887d"
+    "url": "assets/js/9.11b46ea6.js",
+    "revision": "e9960547fa84b484dfd80127dbeb7744"
   },
   {
-    "url": "assets/js/app.1fc208e8.js",
-    "revision": "c3088107dc4c1986392a96e23cccfe3b"
+    "url": "assets/js/app.0a87b07e.js",
+    "revision": "a264ede545989b8409971ec54db4037d"
   },
   {
     "url": "EventType.html",
-    "revision": "f423a2415978eb1d83815504046dd1d3"
+    "revision": "6622a4c78d2b0148baff6c38f354121e"
   },
   {
     "url": "Heartbeat.html",
-    "revision": "0692f06782f1e5228ab882151d379531"
+    "revision": "3ce051aba8662e859caa65602d22d90b"
   },
   {
     "url": "index.html",
-    "revision": "d8bd0d921b33c6d79378b88267e7b320"
+    "revision": "ceef0cf8b14278bdc822e7fd345a7b15"
   },
   {
     "url": "MessageType.html",
-    "revision": "68fc94fc1c91a732258fb636ef812063"
+    "revision": "50b39bf2ea8872ddc6d0e16e0a838e5f"
   },
   {
     "url": "Report.html",
-    "revision": "f16731327d2bece70f7927ca06b49774"
+    "revision": "c9bc791cee9c53cd1d6ab7ef8693f48d"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
