e2ac604b8563edaad9bf9e32dc8213cb67c22818
mamoebot <mamoebot@users.noreply.github.com>
2021-05-07 03:59:53 +0000

    Fix missing property in doc (#336) (d0dbc8bb00639a078b40f9c508713000febbec2e)

--- service-worker.js
+++ service-worker.js
@@ -27,11 +27,11 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "78a4a8d2c21051b781ca728701d0d9cb"
+    "revision": "7502c8ce526c6bd7c33c84ad447c77f3"
   },
   {
     "url": "API.html",
-    "revision": "67fe95c1e94c9d39b3933b249c32704f"
+    "revision": "5707bda6fd80b3030671063d831279b7"
   },
   {
     "url": "assets/css/0.styles.d298e7d4.css",
@@ -46,72 +46,72 @@ self.__precacheManifest = [
     "revision": "867d45d8f9c0da0e3e733dd5e7a8d263"
   },
   {
-    "url": "assets/js/10.9d2758e9.js",
-    "revision": "75eff01a47196549f3906c3809a1cc54"
+    "url": "assets/js/10.536f15cb.js",
+    "revision": "0d23d2b6f0afc0d95f604797876fc063"
   },
   {
-    "url": "assets/js/11.36d9e40a.js",
-    "revision": "b1246b982625f6cb4bedbed2d568c8a4"
+    "url": "assets/js/11.97c4a0b3.js",
+    "revision": "7a9024478e95a07b83971420d511dae3"
   },
   {
-    "url": "assets/js/12.d2794380.js",
-    "revision": "8553107168d8cc28351d7933d5e85bee"
+    "url": "assets/js/12.019aa674.js",
+    "revision": "5baf339f84926486eacec5b903bdd412"
   },
   {
-    "url": "assets/js/2.4a43d499.js",
-    "revision": "15ec0d6280f21d0403b7244542dd7d96"
+    "url": "assets/js/2.10b98dd9.js",
+    "revision": "565320893b0787730e837c9f6965618b"
   },
   {
-    "url": "assets/js/3.1b65837f.js",
-    "revision": "fbf58b9ed28208c69a0dc9c6046051fb"
+    "url": "assets/js/3.394b0e42.js",
+    "revision": "da6df5c47b3054502f717f99064f2b99"
   },
   {
-    "url": "assets/js/4.79e06109.js",
-    "revision": "d3769e05c7a0efdfebe2179e2bb6e3f0"
+    "url": "assets/js/4.581ad1eb.js",
+    "revision": "21cf220186485b613b13a85a0446d080"
   },
   {
-    "url": "assets/js/5.ca660288.js",
-    "revision": "d12cfe72b062e33b2e1f7293f7169cde"
+    "url": "assets/js/5.03f40231.js",
+    "revision": "f91b3a20f085579f3388dc1723807b27"
   },
   {
-    "url": "assets/js/6.330843ad.js",
-    "revision": "75065a966117941b0f9fabcc01313ec5"
+    "url": "assets/js/6.7c4c6923.js",
+    "revision": "3a071d2ec003d0883b944046948e566b"
   },
   {
-    "url": "assets/js/7.017fe42c.js",
-    "revision": "8e0ff14d7a8d14848bd51ff144b7e581"
+    "url": "assets/js/7.0cf748a6.js",
+    "revision": "c69059b15b16593f8440c743ef7cabd8"
   },
   {
-    "url": "assets/js/8.4614edb7.js",
-    "revision": "810d737eb5b49e55ef8c20117c81965e"
+    "url": "assets/js/8.ce38cfba.js",
+    "revision": "cfe783e7155ada73082dc8712e1fad4d"
   },
   {
-    "url": "assets/js/9.00d7c915.js",
-    "revision": "6e4b2630eb70ca0dc5cacb080369a724"
+    "url": "assets/js/9.0813d974.js",
+    "revision": "eb9f248edee9a1c15a2bec578ff3d416"
   },
   {
-    "url": "assets/js/app.0bc14404.js",
-    "revision": "68eea754600369d2608b542cb60387a0"
+    "url": "assets/js/app.c51fdb0e.js",
+    "revision": "be960e1e7a6b70015841fdecb8c438d3"
   },
   {
     "url": "EventType.html",
-    "revision": "1047b43ff160087e4cdc144bf682c346"
+    "revision": "593405bee7bb6d72598e5887b3051d73"
   },
   {
     "url": "Heartbeat.html",
-    "revision": "b89707922791a885ee7e2392063452da"
+    "revision": "0b13c824690d885af694c63e8ab15e32"
   },
   {
     "url": "index.html",
-    "revision": "cd2294badca22b4d91add4993bbd8f31"
+    "revision": "b1270a84893d01ca55b3aa1fc66333fc"
   },
   {
     "url": "MessageType.html",
-    "revision": "d51a0438254e15a07c43d059725c6413"
+    "revision": "bd083af245da541972568c3d8831e8b9"
   },
   {
     "url": "Report.html",
-    "revision": "8940ab24577741213ddf6c05af85f5aa"
+    "revision": "6da9a35e94ed66ba7eab0246b392a541"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
