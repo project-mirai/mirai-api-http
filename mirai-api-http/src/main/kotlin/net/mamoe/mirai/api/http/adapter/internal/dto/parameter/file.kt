5099b3a76e034b1c81a238d4ece9eed8ad3053e6
ryoii <ryoii@foxmail.com>
2021-06-12 17:34:43 +0800

    Fix FileDTO serializer error, close #354

--- mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/adapter/internal/dto/parameter/file.kt
+++ mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/adapter/internal/dto/parameter/file.kt
@@ -1,34 +1,39 @@
 package net.mamoe.mirai.api.http.adapter.internal.dto.parameter
 
+import kotlinx.serialization.Serializable
 import net.mamoe.mirai.api.http.adapter.internal.dto.AuthedDTO
 
-internal open class FileTargetDTO(
-    open val id: String,
-    open val target: Long?,
-    open val group: Long?,
-    open val qq: Long?,
-) : AuthedDTO()
+@Serializable
+internal sealed class FileTargetDTO: AuthedDTO() {
+    abstract val id: String
+    abstract val target: Long?
+    abstract val group: Long?
+    abstract val qq: Long?
+}
 
+@Serializable
 internal data class MkDirDTO(
     override val id: String,
     override val target: Long?,
     override val group: Long?,
     override val qq: Long?,
     val dictionaryName: String,
-) : FileTargetDTO(id, target, group, qq)
+) : FileTargetDTO()
 
+@Serializable
 internal data class RenameFileDTO(
     override val id: String,
     override val target: Long?,
     override val group: Long?,
     override val qq: Long?,
     val renameTo: String,
-) : FileTargetDTO(id, target, group, qq)
+) : FileTargetDTO()
 
+@Serializable
 internal data class MoveFileDTO(
     override val id: String,
     override val target: Long?,
     override val group: Long?,
     override val qq: Long?,
     val moveTo: String,
-) : FileTargetDTO(id, target, group, qq)
+) : FileTargetDTO()
