861ca93a77bd92f4bef02763023965c82fca5b6f
ryoii <ryoii@foxmail.com>
2021-06-12 17:58:05 +0800

    Release 2.0.0

--- mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/HttpApiPluginBase.kt
+++ mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/HttpApiPluginBase.kt
@@ -24,7 +24,7 @@ import java.io.File
  * 主要职责为读取配置文件 [MainSetting] 和 启动具体实现 [MahPluginImpl]
  */
 object HttpApiPluginBase : KotlinPlugin(
-    JvmPluginDescription(id = "net.mamoe.mirai-api-http", version = "2.0-RC2") {
+    JvmPluginDescription(id = "net.mamoe.mirai-api-http", version = "2.0.0") {
         author("ryoii")
         info("Mirai HTTP API Server Plugin")
     }
