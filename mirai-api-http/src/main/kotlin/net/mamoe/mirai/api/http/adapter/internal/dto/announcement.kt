26f5a3b61ddb0854e1123f1a26123e15249027a1
ryoii <ryoii@foxmail.com>
2022-02-14 16:26:04 +0800

    Announcement data model

---
+++ mirai-api-http/src/main/kotlin/net/mamoe/mirai/api/http/adapter/internal/dto/announcement.kt
@@ -0,0 +1,14 @@
+package net.mamoe.mirai.api.http.adapter.internal.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class AnnouncementDTO(
+    val group: GroupDTO,
+    val senderId: Long,
+    val fid: String,
+    val allConfirmed: Boolean,
+    val confirmedMembersCount: Int,
+    val publicationTime: Long,
+)
+
